bf6bb60d2ab87e6c856c26092532396735b1459c
Gabriel Moraga <agarom97@gmail.com>
2020-12-16 16:31:33 +0100

    Creati i file base

---
+++ js/script.js
@@ -0,0 +1,21 @@
+var cognomi = ['Moraga', 'Figueroa', 'Marchesin', 'Bassi', 'Caoduro', 'Passarin', 'Jenkins', 'Buonbarile', 'Jha\'Fall', 'Dianto'];
+
+var numberPresence = false;
+
+// do {
+//   var inputCognome = prompt('Buongiorno. Scriva qui il suo cognome.');
+//
+//   for (var i = 0; i < 10; i++) {
+//     if (inputCognome.includes(i)) {
+//       alert('UN COGNOME CON NUMERI!?');
+//       numberPresence = true;
+//     }
+//   }
+// } while (inputCognome.includes(i));
+//
+//
+// if (!numberPresence) {
+//   console.log(inputCognome);
+// }
+//
+// console.log('ok');

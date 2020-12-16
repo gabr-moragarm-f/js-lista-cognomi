ba59720c816ed9295a65e4d71e23e926686fe67e
Gabriel Moraga <agarom97@gmail.com>
2020-12-16 17:11:40 +0100

    Completo

--- js/script.js
+++ js/script.js
@@ -1,21 +1,33 @@
-var cognomi = ['Moraga', 'Figueroa', 'Marchesin', 'Bassi', 'Caoduro', 'Passarin', 'Jenkins', 'Buonbarile', 'Jha\'Fall', 'Dianto'];
+// array con la lista di cognomi
+var cognomi = ['moraga', 'figueroa', 'marchesin', 'bassi', 'caoduro', 'passarin', 'jenkins', 'buonbarile', 'jha\'fall', 'dianto'];
 
-var numberPresence = false;
+// input dell'utente
+var inputCognome = prompt('Buongiorno. Scriva qui il suo cognome.');
 
-// do {
-//   var inputCognome = prompt('Buongiorno. Scriva qui il suo cognome.');
-//
-//   for (var i = 0; i < 10; i++) {
-//     if (inputCognome.includes(i)) {
-//       alert('UN COGNOME CON NUMERI!?');
-//       numberPresence = true;
-//     }
-//   }
-// } while (inputCognome.includes(i));
-//
-//
-// if (!numberPresence) {
-//   console.log(inputCognome);
-// }
-//
-// console.log('ok');
+inputCognome = inputCognome.toLowerCase(); //standardizzazione dell'input
+
+cognomi.push(inputCognome); // inserimento dell'input nella lista
+
+console.log(cognomi);
+
+cognomi.sort(); // ordine alfabetico della lista
+
+console.log(cognomi);
+
+var listaCognomi = document.getElementById('lista-cognomi')
+
+var cognomiPrecedenti;
+
+// stampa in index.html della lista
+for (var i = 0; i < cognomi.length; i++) {
+  cognomiPrecedenti = listaCognomi.innerHTML
+
+  listaCognomi.innerHTML = cognomiPrecedenti + '<li>' + cognomi[i] + '</li>'
+}
+
+// posizione dell'oggetto nella lista a cui viene sommato 1 per renderla "umana"
+var posizioneLista = document.getElementById('posizione-lista')
+
+posizioneLista.innerHTML = cognomi.indexOf(inputCognome) + 1; // stampa posizione
+
+console.log('ok');
